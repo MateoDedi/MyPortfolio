bfea9cb7868ba82468f89dd0e8148c666e8a9713
MateoDedi <mateod@live.com>
2024-02-14 09:35:58 +0100

    added better links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -49,8 +49,8 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https:www.linkedin.com/in/mateo-dedi"><img src={navIcon1} alt="LinkedIn" /></a>
-                <a href="https://github.com/MateoDedi"><img src={navIcon2} alt="GitHub" /></a>
+                <a href="https:www.linkedin.com/in/mateo-dedi" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
+                <a href="https://github.com/MateoDedi" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="GitHub" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
