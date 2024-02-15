505de1fe8b334859b44038d92597be39423732b7
MateoDedi <mateod@live.com>
2024-02-15 12:16:00 +0100

    fixed minor linter issue

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,8 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
+  // eslint-disable-next-line no-unused-vars
+  const [__, setIndex] = useState(1);
   const toRotate = ["Web Developer", "Software Developer"];
   const period = 2000;
 
