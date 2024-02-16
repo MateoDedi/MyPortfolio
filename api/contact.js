67a37798afb6ef0699dc559e80f6130dc4418273
Mateo Dedi <mateod@live.com>
2024-02-16 23:47:31 +0100

    debug 4

--- api/contact.js
+++ api/contact.js
@@ -4,9 +4,9 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.USER,
     pass: process.env.APP_PASSWORD,
