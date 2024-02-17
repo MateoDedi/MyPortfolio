60609c972f1469b5d3db25555615b15178507315
Mateo Dedi <mateod@live.com>
2024-02-17 03:21:30 +0100

    removed an import of animate.css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,6 @@
 import { React, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
-import "animate.css";
 
 export const Contact = () => {
   const formInitialDetails = {
