a55c4b604ddf08fecc09e1f41b420aca6d20177a
Mateo Dedi <mateod@live.com>
2024-08-14 23:11:27 +0200

    fixed LinkedIn link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="https:www.linkedin.com/in/mateo-dedi" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
+              <a href="https://www.linkedin.com/in/mateo-dedi/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
               <a href="https://github.com/MateoDedi" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="GitHub" /></a>
             </div>
             <p>Thank you for visiting my Portfolio :)</p>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -49,7 +49,7 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https:www.linkedin.com/in/mateo-dedi" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
+                <a href="https://www.linkedin.com/in/mateo-dedi/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
                 <a href="https://github.com/MateoDedi" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="GitHub" /></a>
               </div>
               <HashLink to='#connect'>
