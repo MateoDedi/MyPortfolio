b3bdc389308f7df42e860fdad6a45f12b6d24086
Mateo Dedi <mateod@live.com>
2025-04-20 15:35:24 +0200

    Update description

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -69,11 +69,12 @@ export const Banner = () => {
                 </span>
               </h1>
               <p>
-                👋 Hello! I'm Mateo Dedi, a passionate full stack developer
-                specialized in frontend with a flair for creating visually
-                stunning and user-friendly websites. With a keen eye for design
-                and a love for crafting seamless user experiences, I translate
-                ideas into interactive and responsive web applications!
+                👋 Hello! I'm Mateo Dedi, a passionate full stack developer with a strong foundation in frontend technologies 
+                    and a growing focus on backend development, 
+                    especially with Java. I'm currently expanding my skills through the University of Helsinki's 
+                    Java Programming MOOC, where I'm honing my understanding of object-oriented programming, 
+                    algorithms, and clean code principles. I enjoy creating efficient, scalable software 
+                    solutions and continuously learning new technologies to tackle complex challenges.
               </p>
               <a className="con" href="#connect">
                 <button className="con-btn">
